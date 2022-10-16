bdefc672067bf7e60fcaad027d2f2549537fa6ed
Etienne <guichardetienne@gmail.com>
2022-10-16 12:08:03 +0200

    Added some statistics, in the process of implementing sugar search

--- Assignment2/AIT-assign-RL-code/q_learning_main.py
+++ Assignment2/AIT-assign-RL-code/q_learning_main.py
@@ -1,11 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 import simple_grid
 from q_learning_skeleton import *
 import gym
 
-def act_loop(env, agent, num_episodes):
+def act_loop(env, agent, num_episodes,exploration_reward):
     for episode in range(num_episodes):
         state = env.reset()
-        agent.reset_episode()
+        tot_t = 0
 
         print('---episode %d---' % episode)
         renderit = False
@@ -21,11 +24,12 @@ def act_loop(env, agent, num_episodes):
 
             if printing:
                 print('---stage %d---' % t)
-                agent.report()
+                agent.report(False)
                 print("state:", state)
 
             action = agent.select_action(state)
             new_state, reward, done, info = env.step(action)
+
             if printing:
                 print("act:", action)
                 print("reward=%s" % reward)
@@ -34,10 +38,15 @@ def act_loop(env, agent, num_episodes):
             state = new_state
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
+                tot_t = t+1
+                agent.reset_episode(tot_t)
                 env.render()
-                agent.report()
+                agent.report(False)
                 break
 
+    episode_durations = agent.report(True)
+    plt.plot(range(len(episode_durations)), episode_durations)
+    plt.show()
     env.close()
 
 
@@ -54,6 +63,7 @@ if __name__ == "__main__":
 
     discount = DEFAULT_DISCOUNT
     ql = QLearner(num_o, num_a, discount) #<- QTable
-    act_loop(env, ql, NUM_EPISODES)
+    exploration_rewards = np.ones((num_o,num_o))*10
+    act_loop(env, ql, NUM_EPISODES,exploration_rewards)
 
 
--- Assignment2/AIT-assign-RL-code/q_learning_skeleton.py
+++ Assignment2/AIT-assign-RL-code/q_learning_skeleton.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
-NUM_EPISODES = 1000
+
+NUM_EPISODES = 300
 MAX_EPISODE_LENGTH = 500
 
 
@@ -21,15 +22,17 @@ class QLearner():
         self.learning_rate = learning_rate
         self.num_states = num_states
         self.num_actions = num_actions
-
+        self.episode_durrations = []
         self.q = np.zeros((num_states,num_actions))
         
     # TODO: Select some interesting statistics to keep track of
     # Maybe total reward of the episode
-    def reset_episode(self):
+    def reset_episode(self,episode_duration):
+
         """
         Here you can update some of the statistics that could be helpful to maintain
         """
+        self.episode_durrations.append(episode_duration)
         pass
 
 
@@ -57,17 +60,26 @@ class QLearner():
     # want us to fix this with one of those solutions (probably the first)
     # However I'm not sure, maybe we should ask on Answers EWI
     def select_action(self, state): 
-        selection = random.random()
+        """selection = random.random()
         if selection <= EPSILON:
             return random.randrange(self.num_actions)
         else:
-            return np.argmax(self.q[state][:])
+            return np.argmax(self.q[state][:])"""
+
+        return np.random.choice([random.randrange(self.num_actions),np.argmax(self.q[state][:])],p=[EPSILON,(1-EPSILON)])
 
     # I cannot come up with something to print
-    def report(self):
+    def report(self,final):
         """
         Function to print useful information, printed during the main loop
         """
+        print("Last episode duration: ", self.episode_durrations[-1])
+        print("Average Epiusode duration: ", sum(self.episode_durrations)/len(self.episode_durrations))
+
+        if final:
+            return self.episode_durrations
+
+
         print("---")
 
 
